7f8a73bb975dc93ee29d1d75daea583ac2260e1d
Sumit Gautam <sghost33@gmail.com>
2022-04-20 12:02:46 +1000

    Add socket.io to the server

--- app.js
+++ app.js
@@ -5,12 +5,18 @@ const PORT = process.env.PORT || 3000
 
 const app = express()
 const server = http.createServer(app)
+const io = require('socket.io')(server);
 
 app.use(express.static("public"));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname+ '/public/index.html');
 })
+
+io.on('connection', (socket) => { 
+  console.log('User connected to socket.io server');
+  console.log(socket.id);
+});
 
 server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`)
