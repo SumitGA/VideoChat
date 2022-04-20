9733b31b616bed2bc4fec1b3b3a4ee7d2b954884
Sumit Gautam <sghost33@gmail.com>
2022-04-20 17:21:22 +1000

    add call dialog using javascript, css and dom manipulation

--- public/js/elements.js
+++ public/js/elements.js
@@ -1,4 +1,49 @@
 export const getIncomingCallDialog = (callTypeInfo, acceptCallHandler, rejectCallHandler) => {
-  console.log('gettting incomming call dialog ')
+  const dialog = document.createElement('div');
+  dialog.classList.add('dialog_wrapper');
+  const dialogContent = document.createElement('div');
+  dialogContent.classList.add('dialog_content');
+
+  // Creating title for the dialog using dom manipulation
+  const title = document.createElement('p');
+  title.classList.add('dialog_title');
+  title.innerHTML = `Incoming ${callTypeInfo} call`;
+
+  // Creating image for the dialog using dom manipulation
+  const imageContainer = document.createElement('div');
+  imageContainer.classList.add('dialog_image_container');
+  const image = document.createElement('img');
+  const avatarImagePath = './utils/images/dialogAvatar.png';
+  image.src = avatarImagePath;
+  imageContainer.appendChild(image);
+
+  // Creating button container using dom manipulation
+  const buttonContainer = document.createElement('div');
+  buttonContainer.classList.add('dialog_button_container');
+
+  const acceptCallButton = document.createElement('button');
+  acceptCallButton.classList.add('dialog_accept_call_button');
+  const acceptCallImage = document.createElement('img');
+  acceptCallImage.classList.add('dialog_button_image');
+  const acceptCallImagePath = './utils/images/acceptCall.png';
+  acceptCallImage.src = acceptCallImagePath;
+  acceptCallButton.appendChild(acceptCallImage);
+  buttonContainer.appendChild(acceptCallButton);
+
+  const rejectCallButton = document.createElement('button');
+  rejectCallButton.classList.add('dialog_reject_call_button');
+  const rejectCallImage = document.createElement('img');
+  rejectCallImage.classList.add('dialog_button_image');
+  const rejectCallImagePath = './utils/images/rejectCall.png';
+  rejectCallImage.src = rejectCallImagePath;
+  rejectCallButton.appendChild(rejectCallImage);
+  buttonContainer.appendChild(rejectCallButton);
+
+
+  dialogContent.appendChild(title);
+  dialogContent.appendChild(imageContainer);
+  dialogContent.appendChild(buttonContainer);
+
+  dialog.appendChild(dialogContent);
 }
 
